9ffeaccd51b64c0b01216b0c03d076a3d4ac338c
Jingxian Chai <jingxianchai01@gmail.com>
2022-02-24 15:04:02 -0600

    git push test

--- msweeper/src/Leaderboard.js
+++ msweeper/src/Leaderboard.js
@@ -7,6 +7,7 @@ const Leaderboard = () => {
       <h3>1. ... 0 secs</h3>
       <h3>2. ... 0 secs</h3>
       <h3>3. ... 0 secs</h3>
+      <h3>4. ... 0 secs</h3>
       <h1 className="underline text-4xl my-5 font-bold">Intermediate</h1>
       <h3>1. ... 0 secs</h3>
       <h3>2. ... 0 secs</h3>
