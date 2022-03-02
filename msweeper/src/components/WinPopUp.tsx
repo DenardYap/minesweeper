a390ed2df27c2895c1add86c4e1d06a7dba72394
DenardYap <mameehy@hotmail.com>
2022-03-02 00:13:38 -0500

    fixed WinPopUp for mobile

--- msweeper/src/components/WinPopUp.tsx
+++ msweeper/src/components/WinPopUp.tsx
@@ -10,14 +10,14 @@ const WinPopUp: React.FC<WinPopUpProps> = ({gameWon}) => {
     gameWon ? (
         <div 
         className="flex flex-col absolute items-center rounded
-        bg-slate-600 text-slate-100 text-center text-[1.2vw] 
-        m-auto w-[20vw] h-[17.5vh] left-0 right-0 top-0 bottom-0">
+        bg-slate-600 text-slate-100 text-center sm:text-[1.2vw] ssm:text-[3vw] 
+        m-auto ssm:w-[80vw] sm:w-[20vw] h-[17.5vh] left-0 right-0 top-0 bottom-0">
         <label className="leading-[10vh] bg-slate-600">
             Let's be on the leaderboard 😎
         </label>
         <input
         placeholder={"your name..."}
-            className="rounded shadow-2xl w-[90%] h-[5vh] px-[0.5vw] bg-slate-400 text-white hover:bg-slate-200 hover:text-black hover:placeholder-black placeholder-white "
+            className="rounded shadow-2xl w-[90%] h-[5vh] sm:px-[0.5vw] ssm:px-[2vw] bg-slate-400 text-white hover:bg-slate-200 hover:text-black hover:placeholder-black placeholder-white "
             type="text"
             autoFocus
         />
