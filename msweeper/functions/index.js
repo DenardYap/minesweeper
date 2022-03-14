14746ac9776244d3eebc0f41587f67677a0dd3ad
DenardYap <mameehy@hotmail.com>
2022-03-14 13:56:10 -0400

    asdasd

--- msweeper/functions/index.js
+++ msweeper/functions/index.js
@@ -24,8 +24,8 @@ app.use(express.json());
 
 // some middleware for testing with localhost
 let allowCrossDomain = function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "https://msweeper.com");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://msweeper.com");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Authorization, content-type");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
